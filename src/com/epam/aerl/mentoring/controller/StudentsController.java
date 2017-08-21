d2df48dcce309e6a4335a52749d6cc23ca1fac90
Yauheni Dubouski <Yauheni_Dubouski@epam.com>
2017-08-21 17:23:34 +0300

    Modify controller class. Generate students and employers in controller.

--- src/com/epam/aerl/mentoring/controller/StudentsController.java
+++ src/com/epam/aerl/mentoring/controller/StudentsController.java
@@ -1,18 +1,34 @@
 package com.epam.aerl.mentoring.controller;
 
+import java.util.List;
+
+import com.epam.aerl.mentoring.entity.Student;
 import com.epam.aerl.mentoring.exception.ServiceException;
+import com.epam.aerl.mentoring.exception.StudentsGeneratorException;
+import com.epam.aerl.mentoring.filter.EmployerFilter;
 import com.epam.aerl.mentoring.service.StudentsService;
 import com.epam.aerl.mentoring.type.ErrorMessage;
+import com.epam.aerl.mentoring.util.EmployersGenerator;
 import com.epam.aerl.mentoring.util.Printer;
+import com.epam.aerl.mentoring.util.StudentsGenerator;
 
 public class StudentsController {
-
+	private static final int STUDENTS_COUNT = 50;
+	
+	private EmployersGenerator employersGenerator = new EmployersGenerator();
+	private StudentsGenerator studentsGenerator = new StudentsGenerator();
+	
 	private StudentsService service = new StudentsService();
 	
 	public void takeStudents() {
 		try {
-			service.takeStudentsFromUniversity();
+			List<EmployerFilter> employers = employersGenerator.generate();
+			List<Student> students = studentsGenerator.generateStudents(STUDENTS_COUNT);
+			
+			service.takeStudentsFromUniversity(employers, students);
 		} catch (ServiceException e) {
+			Printer.printErrorMessage(ErrorMessage.getByCode(e.getCode()));
+		} catch (StudentsGeneratorException e) {
 			Printer.printErrorMessage(ErrorMessage.getByCode(e.getCode()));
 		}
 	}
--- src/com/epam/aerl/mentoring/service/StudentsService.java
+++ src/com/epam/aerl/mentoring/service/StudentsService.java
@@ -5,35 +5,22 @@ import java.util.List;
 import com.epam.aerl.mentoring.entity.Student;
 import com.epam.aerl.mentoring.exception.EmplyerFilterException;
 import com.epam.aerl.mentoring.exception.ServiceException;
-import com.epam.aerl.mentoring.exception.StudentsGeneratorException;
 import com.epam.aerl.mentoring.filter.EmployerFilter;
 import com.epam.aerl.mentoring.type.ErrorMessage;
-import com.epam.aerl.mentoring.util.EmployersGenerator;
 import com.epam.aerl.mentoring.util.Printer;
-import com.epam.aerl.mentoring.util.StudentsGenerator;
 
 public class StudentsService {
-	private static final int STUDENTS_COUNT = 50;
 	private static final String CAPTION = "Students which will remain:";
 	
-	private static final String NOT_GENERATED_STUDENTS_ERR = "Can not generate students.";
 	private static final String NOT_FILTERED_STUDENTS_ERR = "Can not filter list of students.";
 	
-	private EmployersGenerator employersGenerator = new EmployersGenerator();
-	private StudentsGenerator studentsGenerator = new StudentsGenerator();
-	
-	public void takeStudentsFromUniversity() throws ServiceException {
+	public void takeStudentsFromUniversity(List<EmployerFilter> employers, List<Student> students) throws ServiceException {
 		try {
-			List<EmployerFilter> employers = employersGenerator.generate();
-			List<Student> students = studentsGenerator.generateStudents(STUDENTS_COUNT);
-			
 			for(EmployerFilter employer : employers) {
 				employer.takeAway(students);
 			}
 			
 			printRemainStudents(students);
-		} catch (StudentsGeneratorException e) {
-			throw new ServiceException(ErrorMessage.NOT_GENRATE_ERROR.getCode(), NOT_GENERATED_STUDENTS_ERR, e);
 		} catch (EmplyerFilterException e) {
 			throw new ServiceException(ErrorMessage.NOT_FILTERED_ERROR.getCode(), NOT_FILTERED_STUDENTS_ERR, e);
 		}
